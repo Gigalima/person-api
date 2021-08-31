7d21013fe43c178d76adedfd377c9ba7be025c31
gigalima <douglima@outlook.com>
2021-08-31 14:25:09 -0300

    Adiciona estrutura projeto person-api

---
+++ person-api/src/main/java/com/gigalima/personapi/controller/PersonController.java
@@ -0,0 +1,15 @@
+package com.gigalima.personapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/people")
+public class PersonController {
+	
+	@RequestMapping
+	public String ola() {
+		return "Olá Projeto";
+	}
+
+}
